99c815aae5ba687f83421dbee57956f23ed80bec
shervina22001 <shervina22001@mail.unpad.ac.id>
2024-06-14 09:41:07 +0700

    Fixed Training

--- training.py
+++ training.py
@@ -1,32 +1,33 @@
-import cv2, os
-import numpy as np
-from PIL import Image 
+import cv2
 import os
+import numpy as np
+from PIL import Image
 
 def assure_path_exists(path):
     dir = os.path.dirname(path)
-    if not os.exists(dir):
+    if not os.path.exists(dir):
         os.makedirs(dir)
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector - cv2.cascadeClassifier("haarcascade_frontalface_default.xml");
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 def getImageAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
     faceSamples = []
     ids = []
     for imagePath in imagePaths:
-        PIL_ing = Image.open(ImagePath).convert('L')
+        PIL_img = Image.open(imagePath).convert('L')
         img_numpy = np.array(PIL_img, 'uint8')
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
-        for (x,y,w,h) in faces:
+        for (x, y, w, h) in faces:
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
 
 faces,ids = getImageAndLabels('dataset')
 recognizer.train(faces, np.array(ids))
-assure_path_exists('traoner/')
-recognizer.save('traineer/')
+
+assure_path_exists('trainer/')
 recognizer.save('trainer/trainer.yml')
                                                 
